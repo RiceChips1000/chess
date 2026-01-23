1e1c17bd92c5928e7ce35e78feb8516ad53e3480
rice1000 <rhysmartinsen@gmail.com>
2026-01-22 17:40:51 -0700

    fixed bracket stuff

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -39,8 +39,8 @@ public class ChessPosition {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof ChessPosition)) return false;
+        if (this == o) {return true;}
+        if (!(o instanceof ChessPosition)) {return false;}
         ChessPosition that = (ChessPosition) o;
         return row == that.row && col == that.col;
     }
