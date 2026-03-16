7a0ac29d9e6ba6010b4cd72b5f9ac9eaf38cb339
rice1000 <rhysmartinsen@gmail.com>
2026-03-16 14:14:14 -0600

    Make sure branch correct

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -7,6 +7,7 @@ public class ClientMain {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
         //Websocket change files
+
         //pregame start
 
 
