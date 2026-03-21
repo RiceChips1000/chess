59e0c23faa2b746a14a77d7ed9b44788771c15ce
rice1000 <rhysmartinsen@gmail.com>
2026-03-21 16:58:59 -0600

    About half of the positive serverFacadeTests :)

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,29 +1,68 @@
 package client;
 
+import model.*;
 import org.junit.jupiter.api.*;
+
 import server.Server;
 
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ServerFacadeTests {
 
+
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
-    public static void init() {
+    public static void init()  {
+
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
     }
 
     @AfterAll
-    static void stopServer() {
+    static void stopServer()    {
+
         server.stop();
     }
 
+    @BeforeEach
+    void clearDatabase() throws Exception {
+
+        facade.clear();
+    }
+
+    // ===== POSITIVE TESTS =======
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void registerPositive() throws Exception {
+
+        var authData = facade.register("player1", "password", "p1@email.com");
+        assertNotNull(authData);
+        assertNotNull(authData.authToken());
+
+        assertTrue(authData.authToken().length() > 10);
+        assertEquals("player1", authData.username());
+    }
+
+    @Test
+    void loginPositive() throws Exception
+    {
+        facade.register("player1", "password", "p1@email.com");
+        var authData = facade.login("player1", "password");
+        assertNotNull(authData);
+
+        assertNotNull(authData.authToken());
+        assertEquals("player1", authData.username());
+    }
+
+    @Test
+    void logoutPositive() throws Exception {
+        var authData = facade.register("player1", "password", "p1@email.com");
+        assertDoesNotThrow(() -> facade.logout(authData.authToken()));
     }
 
 }
