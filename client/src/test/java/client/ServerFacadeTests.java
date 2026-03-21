5b8a12604fa5cdd0b2d7961d8d1590d6d2b4f92e
rice1000 <rhysmartinsen@gmail.com>
2026-03-21 17:09:35 -0600

    Should be all the positive tests I need Might add more later just to be sure

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -62,7 +62,47 @@ public class ServerFacadeTests {
     @Test
     void logoutPositive() throws Exception {
         var authData = facade.register("player1", "password", "p1@email.com");
+
         assertDoesNotThrow(() -> facade.logout(authData.authToken()));
     }
 
+    @Test
+    void createGamePositive() throws Exception {
+        var authData = facade.register("player1", "password", "p1@email.com");
+
+        var result = facade.createGame(authData.authToken(), "TestGame");
+        assertNotNull(result);
+        assertNotNull(result.gameID());
+        assertTrue(result.gameID() > 0);
+
+
+    }
+
+    @Test
+    void listGamesPositive() throws Exception {
+
+
+        var authData = facade.register("player1", "password", "p1@email.com");
+        facade.createGame(authData.authToken(), "Game1");
+        facade.createGame(authData.authToken(), "Game2");
+        var games = facade.listGames(authData.authToken());
+        assertNotNull(games);
+        assertEquals(2, games.length);
+
+    }
+
+    @Test
+    void joinGamePositive() throws Exception {
+        var authData = facade.register("player1", "password", "p1@email.com");
+        var gameResult = facade.createGame(authData.authToken(), "TestGame");
+        assertDoesNotThrow(() ->
+                facade.joinGame(authData.authToken(), "WHITE", gameResult.gameID()));
+
+    }
+
+    @Test
+    void clearPositive() throws Exception {
+        facade.register("player1", "password", "p1@email.com");
+        assertDoesNotThrow(() -> facade.clear());
+    }
 }
